687c0ed05a7e564c2ae2a9912841a375141daa33
Noach Valle <noach1977@gmail.com>
2024-12-27 15:55:17 -0800

    intial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import Frontpage from "../../Components/frontpage";
+export default function Home() {
+  return (
+    <div className="flex justify-evenly items-center">
+      <Frontpage/>
+    </div>
+  );
+}
