2d135ad833888c848807f97e6d4812c18b9d3bea
Noach Valle <noach1977@gmail.com>
2025-01-01 20:36:53 -0800

    this is the footer design for a Portfolio website

--- Components/footer.tsx
+++ Components/footer.tsx
@@ -1,35 +1,31 @@
 
+
 const Footer = () => {
     return (
-        <div>
-            <footer className=" rounded-lg shadow m-4">
-                <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-                    <div className="sm:flex sm:items-center sm:justify-between">
-                        <a className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">First Website</span>
-                        </a>
-                        <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+        <footer className="flex flex-col items-center rounded-lg shadow m-4">
+            <ul className="flex items-center mb-6  gap-3">
+                <li><img className="rounded" src="https://cdn-icons-png.flaticon.com/512/124/124021.png" width={35}/></li>
+                <li><img className = "rounded" src="https://cdn-icons-png.freepik.com/256/15707/15707869.png?semt=ais_hybrid" width={35}/></li>
+                <li><img className="rounded" src = "https://cdn-icons-png.flaticon.com/512/3291/3291667.png" width={35}/></li>
+                <li><img className = "rounded"src="https://cdn.pixabay.com/photo/2022/06/05/07/04/person-7243410_1280.png" width={35}/></li>
+            </ul>
+            <ul className="flex items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                <li>
+                    <a href="#" className="hover:underline me-4 md:me-6">Twitter</a>
+                </li>
+                <li>
 
-                            <li>
-                                <a href="#" className="hover:underline me-4 md:me-6">About</a>
-                            </li>
-                            <li>
-
-                                <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
-                            </li>
-                            <li>0.
-                                <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
-                            </li>
-                            <li>
-                                <a href="#" className="hover:underline">Contact</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-                    <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">©2024 <a className="hover:underline">Noach Valle™</a>. All Rights Reserved.</span>
-                </div>
-            </footer>
-        </div>
+                    <a href="#" className="hover:underline me-4 md:me-6">Instagram</a>
+                </li>
+                <li>
+                    <a href="#" className="hover:underline me-4 md:me-6">Github</a>
+                </li>
+                <li>
+                    <a href="#" className="hover:underline">Contact</a>
+                </li>
+            </ul>
+            
+        </footer>
     );
 }
 
