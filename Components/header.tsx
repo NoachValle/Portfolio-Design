11b77b1b6ed62ba1e68c84e10c48ad766e3ad9c8
Noach Valle <noach1977@gmail.com>
2025-01-08 17:16:10 -0800

    What I did here is create a Usestate so that the User will be able to interact with the Hamburger bar on mobile

--- Components/header.tsx
+++ Components/header.tsx
@@ -1,31 +1,43 @@
+"use client"
+
 import Link from 'next/link';
+import { useState } from 'react';
+
 const Header = () => {
+  const [Dropdown, SetDropDown] = useState(false);
+
+  const HamburgerDropDown = ()=>{
+    SetDropDown(!Dropdown);
+  }
   return (
     <nav>
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a className=" flex items-center space-x-3 rtl:space-x-reverse">
           <span className="self-center text-4xl font-semibold whitespace-nowrap dark:text-white">Isobelle Holden</span>
         </a>
-        <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm  rounded-lg md:hidden focus:outline-none focus:ring-2 " aria-controls="navbar-default" aria-expanded="false">
+        <button onClick={HamburgerDropDown} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm  rounded-lg md:hidden focus:outline-none focus:ring-2 " aria-controls="navbar-default" aria-expanded="false">
           <span className="sr-only">Open main menu</span>
           <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
           </svg>
         </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+        <div className= {`${Dropdown ? "hidden" : ""} w-full md:block md:w-auto`} id="navbar-default">
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4  rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
-          
-            <li>
-              <a href="#" className="block py-2 px-3 md:hover:bg-transparent  dark:text-white  dark:hover:text-white md:dark:hover:bg-transparent">Portfolio</a>
+
+            <li className="block py-2 px-3 md:hover:bg-transparent  dark:text-white  dark:hover:text-white md:dark:hover:bg-transparent">
+              <Link href="/">Portfolio</Link>
             </li>
-            <li>
-              <a href="#" className="block py-2 px-3 md:hover:bg-transparent  dark:text-white  dark:hover:text-white md:dark:hover:bg-transparent">Experience</a>
+            <li className="block py-2 px-3 md:hover:bg-transparent  dark:text-white  dark:hover:text-white md:dark:hover:bg-transparent">
+
+              <Link href="/Experience">Experience</Link>
             </li>
-            <li>
-              <a href="#" className="block py-2 px-3 md:hover:bg-transparent  dark:text-white dark:hover:text-white md:dark:hover:bg-transparent">Projects</a>
+            <li className="block py-2 px-3 md:hover:bg-transparent  dark:text-white dark:hover:text-white md:dark:hover:bg-transparent">
+
+              <Link href="/Projects">Projects</Link>
             </li>
-            <li>
-              <a href="#" className="block py-2 px-3  md:hover:bg-transparent  dark:text-white dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+            <li className="block py-2 px-3  md:hover:bg-transparent  dark:text-white dark:hover:text-white md:dark:hover:bg-transparent">
+
+              <Link href="/Contact">Contact</Link>
             </li>
           </ul>
         </div>
