e0d00e79901d06582f37e97715c78406058db50f
Noach Valle <noach1977@gmail.com>
2025-02-09 15:05:21 -0800

    the pauseTime function is being used here

--- src/app/actions/receiveFormData.tsx
+++ src/app/actions/receiveFormData.tsx
@@ -1,6 +1,9 @@
 "use server";
 
+import pauseTime from "./pauseTime";
+
 const receiveFormData = async (prevState: any,formData: FormData) => {
+    await pauseTime(2000);
     return { message: 'Success, your message has been sent!',};
 }
 export default receiveFormData;
